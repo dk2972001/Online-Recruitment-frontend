ad00d0405d8880b509b5249b89eec784ded6087a
iamwatchdogs <shamith3011022@gmail.com>
2024-05-16 21:05:31 +0530

    Finished employer to company, company to job and job to joblist

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent implements OnInit {
   companyForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.companyForm = this.formBuilder.group({
@@ -17,15 +18,25 @@ export class CompanyComponent implements OnInit {
       company_type: ['', Validators.required],
       company_name: ['', Validators.required],
       company_description: ['', Validators.required],
-      company_address: ['', Validators.required]
+      company_address: ['', Validators.required],
     });
   }
 
   addCompany() {
     if (this.companyForm.valid) {
       console.log('Company added:', this.companyForm.value);
+      alert('Added Company details');
+      this.proceedingToJobs();
     } else {
       console.log('Form is not valid');
     }
+  }
+
+  proceedingToJobs() {
+    this.router.navigate(['/job']);
+  }
+
+  navigateBack() {
+    this.router.navigate(['/']);
   }
 }
--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-job',
   templateUrl: './job.component.html',
-  styleUrls: ['./job.component.css']
+  styleUrls: ['./job.component.css'],
 })
 export class JobComponent implements OnInit {
   jobForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.jobForm = this.formBuilder.group({
@@ -18,15 +19,25 @@ export class JobComponent implements OnInit {
       salary: ['', Validators.required],
       job_type: ['', Validators.required],
       job_description: ['', Validators.required],
-      job_vacancy: ['', [Validators.required, Validators.min(1)]]
+      job_vacancy: ['', [Validators.required, Validators.min(1)]],
     });
   }
 
   addJob() {
     if (this.jobForm.valid) {
       console.log('Job added:', this.jobForm.value);
+      alert('Added new Job');
+      this.proceedingToJobList();
     } else {
       console.log('Form is not valid');
     }
+  }
+
+  proceedingToJobList() {
+    this.router.navigate(['/job-list']);
+  }
+
+  navigateBack() {
+    this.router.navigate(['/']);
   }
 }
