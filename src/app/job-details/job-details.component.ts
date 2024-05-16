f5a357081b6a12663e442bfe4022cfd46dbab845
iamwatchdogs <shamith3011022@gmail.com>
2024-05-16 19:51:36 +0530

    Finished job-details component

--- src/app/job-details/job-details.component.ts
+++ src/app/job-details/job-details.component.ts
@@ -7,6 +7,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class JobDetailsComponent implements OnInit {
   jobId: string = '';
+  job = {
+    jobName: 'HTML Designer',
+    salary: 'No disclosed',
+    applyLink: '#',
+    jobType: 'Software',
+    jobDescription: 'HTML tech stack',
+    jobVaccancy: 'Almost Full',
+    companyName: 'Webex',
+    companyType: 'Online',
+    companyDescr: 'we make website for people',
+    companyAddress: 'Remote',
+  };
 
   constructor(private route: ActivatedRoute) {}
 
