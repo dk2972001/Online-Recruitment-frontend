9715388bad0c9d8d58012f5721cff8077e36e4d3
Anshika-03 <anshikaanshika01234@gmail.com>
2024-05-25 17:50:37 +0530

    finished appointment page and done routing for student

--- src/app/job-availability/job-availability.component.ts
+++ src/app/job-availability/job-availability.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  AbstractControl,
-  ValidationErrors,
-} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-job-availability',
   templateUrl: './job-availability.component.html',
-  styleUrl: './job-availability.component.css',
+  styleUrls: ['./job-availability.component.css'], // Ensure this is 'styleUrls'
 })
 export class JobAvailabilityComponent implements OnInit {
   jobAvailabilityForm!: FormGroup;
   jobId: string = '';
 
-  constructor(private fb: FormBuilder, private route: ActivatedRoute) {}
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router // Inject the Router service
+  ) {}
 
   validateFile(control: AbstractControl): { [key: string]: any } | null {
     const file = control.value;
@@ -31,21 +29,31 @@ export class JobAvailabilityComponent implements OnInit {
   ngOnInit(): void {
     this.jobAvailabilityForm = this.fb.group({
       name: ['', [Validators.required]],
-      email: ['', Validators.required, Validators.email],
+      email: ['', [Validators.required, Validators.email]],
       phoneno: ['', [Validators.pattern('[6789][0-9]{9}')]],
       yearofpassing: ['', [Validators.required]],
-      percentage: [
-        '',
-        [Validators.required, Validators.min(0), Validators.max(100)],
-      ],
-      language: ['', Validators.pattern('([a-zA-Z]+,?)*')],
-      skills: ['', Validators.pattern('([a-zA-Z]+,?)*')],
-      project: ['', Validators.pattern('([a-zA-Z]+,?)*')],
+      percentage: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
+      language: ['', [Validators.pattern('([a-zA-Z]+,?)*')]],
+      skills: ['', [Validators.pattern('([a-zA-Z]+,?)*')]],
+      project: ['', [Validators.pattern('([a-zA-Z]+,?)*')]],
       resume: ['', [Validators.required, this.validateFile]],
     });
     this.jobId = this.route.snapshot.paramMap.get('jobId')!;
   }
-  submit() {
-    console.log(this.jobAvailabilityForm.value);
+
+  onSubmit() {
+    if (this.jobAvailabilityForm.valid) {
+      console.log('Form Submission', this.jobAvailabilityForm.value);
+      alert('Successfully Added');
+      this.redirectToJobsList();
+    }
+  }
+
+  redirectToJobsList() {
+    this.router.navigate(['/appointment-sch']);
+  }
+
+  navigateBack() {
+    this.router.navigate(['/']);
   }
 }
