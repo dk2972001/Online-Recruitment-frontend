c2c4bcd2363755d839934c6e65b8bd6f511c7f01
iamwatchdogs <shamith3011022@gmail.com>
2024-05-26 22:08:49 +0530

    Add submit functionality to college component

--- src/app/college/college.component.ts
+++ src/app/college/college.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CollegeService } from '../college.service';
-import { College } from '../college.model';  // Ensure you have a College model
+import { College } from '../college.model'; // Ensure you have a College model
 
 @Component({
   selector: 'app-college',
   templateUrl: './college.component.html',
-  styleUrls: ['./college.component.css']
+  styleUrls: ['./college.component.css'],
 })
 export class CollegeComponent {
   collegeForm!: FormGroup;
@@ -34,16 +34,28 @@ export class CollegeComponent {
 
   onSubmit() {
     if (this.collegeForm.valid) {
+      const collegeData: College = {
+        collegeId: this.collegeForm.value.college_id,
+        collegeName: this.collegeForm.value.college_name,
+        collegeAddress: this.collegeForm.value.college_address,
+        collegeDescp: this.collegeForm.value.college_description,
+      };
 
+      this.collegeService.addCollege(collegeData).subscribe(
+        (response) => {
+          console.log('College added:', response);
+          alert('Successfully Added College Details');
+          this.redirectToJobsList();
+        },
+        (error) => {
+          console.error('Error adding college:', error);
+        }
+      );
     }
   }
 
   redirectToJobsList() {
     this.router.navigate(['/job-list']);
-  }
-  navigateToLogin() {
-    // Navigate to the login page
-    this.router.navigate(['/login']);
   }
 
   navigateBack() {
