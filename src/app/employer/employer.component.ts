fc8e250b7ea87df4790357e34f4b210afca629ba
iamwatchdogs <shamith3011022@gmail.com>
2024-05-26 23:28:53 +0530

    Integrated employer to the backend

--- src/app/employer/employer.component.ts
+++ src/app/employer/employer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { EmployerService } from '../services/employer.service'; 
+import { Employer } from '../models/employer.model';
 
 @Component({
   selector: 'app-employer',
@@ -10,27 +12,38 @@ import { Router } from '@angular/router';
 export class EmployerComponent implements OnInit {
   employerForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private employerService: EmployerService
+  ) {}
 
   ngOnInit(): void {
     this.employerForm = this.formBuilder.group({
-      employer_id: ['', Validators.required],
-      employer_name: ['', Validators.required],
-      employer_post: ['', Validators.required],
-      employer_address: ['', Validators.required],
-      employer_mobile: [
+      employerId: ['', Validators.required],
+      employerName: ['', Validators.required],
+      employerPost: ['', Validators.required],
+      employerAddress: ['', Validators.required],
+      employerMobile: [
         '',
         [Validators.required, Validators.pattern('^[0-9]+$')],
-      ], // Added simple regex for mobile number validation
-      employer_email: ['', [Validators.required, Validators.email]],
+      ],
+      employerEmail: ['', [Validators.required, Validators.email]],
     });
   }
 
   addEmployer() {
     if (this.employerForm.valid) {
-      console.log('Employer added:', this.employerForm.value);
-      alert('Added employeer details');
-      this.proceedingToCompany();
+      const employer: Employer = this.employerForm.value;
+      this.employerService.addEmployer(employer).subscribe(
+        (response) => {
+          console.log('Employer added successfully', response);
+          this.proceedingToCompany();
+        },
+        (error) => {
+          console.log('Error while adding employer', error);
+        }
+      );
     } else {
       console.log('Form is not valid');
     }
